9b9b498e8caf2aef4a5069a5023229688f920158
Alexander Golovanov <Golovanov12345@gmail.com>
2019-09-18 02:19:32 +0300

    add smth

--- cf_scripts/qwe.sh
+++ cf_scripts/qwe.sh
@@ -9,21 +9,7 @@ rm -rf $name
 mkdir $name
 cd $name
 cp ../template.cpp qwe.cpp
-cp ../compile.sh .
-cp ../run.sh .
-# cp ../make_new_folders.sh .
 cp ../parse_tasks.py .
 cp ../check.sh .
-
-# for i in {A..E}
-# do
-# 	mkdir $i;
-# 	cp qwe.cpp $i;
-# 	cp compile.sh $i;
-# 	cp run.sh $i;
-# 	cp check.sh $i;
-# done;
-
-# rm qwe.cpp compile.sh run.sh
 
 echo $name
