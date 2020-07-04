d3faff777657e3729912c40de21cd1852e5684d9
Golovanov399 <Golovanov12345@gmail.com>
2020-07-04 17:39:31 +0300

    Rewrite parse_tasks.py in python3

--- cf_scripts/parse_tasks.py
+++ cf_scripts/parse_tasks.py
@@ -1,34 +1,34 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import re
 import requests
 import sys
 import os
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from shutil import copyfile
 
 if len(sys.argv) < 2:
-	print "Need contest number"
+	print("Need contest number")
 	sys.exit(0)
 
 number = sys.argv[1]
 
 r = requests.get("http://codeforces.com/contest/" + number + "/problems")
 s = r.text
-soup = BeautifulSoup(s)
+soup = BeautifulSoup(s, "html.parser")
 probs = soup.findAll("div", {"class": "problemindexholder"})
 for prob in probs:
 	index = prob["problemindex"]
 	if not os.path.exists(index):
 		os.makedirs(index)
 	samples = prob.find("div", {"class": "sample-test"})
-	io = zip(samples.findAll("div", {"class": "input"}),
-			 samples.findAll("div", {"class": "output"}))
+	io = list(zip(samples.findAll("div", {"class": "input"}),
+				  samples.findAll("div", {"class": "output"})))
 	for i in range(len(io)):
 		inp, out = io[i]
 		with open("%s/in%d.in" % (index, i), "w") as f:
-			print >> f, inp.find("pre").text.strip()
+			print(inp.find("pre").text.strip(), file=f)
 		with open("%s/out%d.out" % (index, i), "w") as f:
-			print >> f, out.find("pre").text.strip()
+			print(out.find("pre").text.strip(), file=f)
 	for file in ["qwe.cpp", "check.sh"]:
 		copyfile(file, index + "/" + file)
