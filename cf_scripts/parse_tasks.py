7bb4896cb4d4794e27a8913bd317f9efe6187f7d
Alexander Golovanov <Golovanov12345@gmail.com>
2022-08-18 15:08:42 +0300

    Update parse tasks script

--- cf_scripts/parse_tasks.py
+++ cf_scripts/parse_tasks.py
@@ -13,7 +13,16 @@ if len(sys.argv) < 2:
 
 number = sys.argv[1]
 
-r = requests.get("http://codeforces.com/contest/" + number + "/problems")
+
+def sample_text(sample):
+	divs = sample.findAll("div", {"class": "test-example-line"})
+	if divs:
+		return "\n".join([test.text.strip() for test in divs if test.text.strip()])
+	else:
+		return sample.text.strip()
+
+
+r = requests.get("http://codeforces.com/contest/" + number + "/problems", allow_redirects=True)
 s = r.text
 soup = BeautifulSoup(s, "html.parser")
 probs = soup.findAll("div", {"class": "problemindexholder"})
@@ -22,13 +31,14 @@ for prob in probs:
 	if not os.path.exists(index):
 		os.makedirs(index)
 	samples = prob.find("div", {"class": "sample-test"})
-	io = list(zip(samples.findAll("div", {"class": "input"}),
-				  samples.findAll("div", {"class": "output"})))
+	io = zip(samples.findAll("div", {"class": "input"}),
+			 samples.findAll("div", {"class": "output"}))
+	io = list(io)
 	for i in range(len(io)):
 		inp, out = io[i]
 		with open("%s/in%d.in" % (index, i), "w") as f:
-			print(inp.find("pre").text.strip(), file=f)
+			print(sample_text(inp.find("pre")), file=f)
 		with open("%s/out%d.out" % (index, i), "w") as f:
-			print(out.find("pre").text.strip(), file=f)
+			print(sample_text(inp.find("pre")), file=f)
 	for file in ["qwe.cpp", "check.sh"]:
 		copyfile(file, index + "/" + file)
