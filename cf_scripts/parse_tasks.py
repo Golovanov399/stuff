6555f4666dd6b03e8d3853402ef0787f5f27ba97
Alexander Golovanov <Golovanov12345@gmail.com>
2022-11-22 19:50:35 +0300

    Add anti-anti-scrap protection

--- cf_scripts/parse_tasks.py
+++ cf_scripts/parse_tasks.py
@@ -4,6 +4,7 @@ import re
 import requests
 import sys
 import os
+import stat
 from bs4 import BeautifulSoup
 from shutil import copyfile
 
@@ -25,6 +26,30 @@ def sample_text(sample):
 r = requests.get("http://codeforces.com/contest/" + number + "/problems", allow_redirects=True)
 s = r.text
 soup = BeautifulSoup(s, "html.parser")
+
+if soup.text == "Redirecting... Please, wait.":
+	print("Bypassing anti-scrap protection...")
+	scr = soup.findAll("script")[-1].string
+	scr = scr[scr.index("var a=toNumbers"):].split(';')
+	line = scr[0]
+	abc = []
+	while "toNumbers" in line:
+		i = line.index("toNumbers")
+		line = line[i+11:]
+		abc.append(line[:line.index('"')])
+	from Crypto.Cipher import AES
+	def to_numbers(x):
+		return bytes(int(x[i:i+2], 16) for i in range(0, len(x), 2))
+	key, iv, cipher = map(to_numbers, abc)
+	aes = AES.new(key, AES.MODE_CBC, iv)
+	rcpc = aes.decrypt(cipher).hex()
+	print(f"RCPC = {rcpc}")
+	url = scr[-2]
+	url = url[url.index('"')+1:-1]
+	r = requests.get(url, cookies={"RCPC": rcpc})
+	s = r.text
+	soup = BeautifulSoup(s, "html.parser")
+
 probs = soup.findAll("div", {"class": "problemindexholder"})
 for prob in probs:
 	index = prob["problemindex"]
@@ -41,4 +66,6 @@ for prob in probs:
 		with open("%s/out%d.out" % (index, i), "w") as f:
 			print(sample_text(out.find("pre")), file=f)
 	for file in ["qwe.cpp", "check.sh"]:
-		copyfile(file, index + "/" + file)
+		newfile = index + "/" + file
+		copyfile(file, newfile)
+		os.chmod(newfile, os.stat(newfile).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
